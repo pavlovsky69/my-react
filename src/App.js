34fa16ad6d08a9202e8b3049caaa711f195eb695
pavlovsky69 <pavlovsky69@gmail.com>
2023-07-11 14:50:29 +0300

    HW 2 finish

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import './App.css';
-import {PostContainer} from "./components/PostContainer.js";
 import {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <PostContainer/>
-    </div>
-  );
-}
+import {Launches} from "./components/Launches/Launches";
+import {Users} from "./components/Users/Users";
+import {UserPosts} from "./components/Users/Posts/Posts";
+import {Posts} from "./components/Posts/Post";
 
-export default App;
+const App = () => {
+    const [userId, setUserId] = useState(null);
+    return (
+        <div>
+            <Posts/>
+            {/*<Launches/>*/}
+            {/*<Users setUserId={setUserId}/>*/}
+            {/*{userId && <UserPosts userId={userId}/>}*/}
+        </div>
+    );
+};
+
+export {App};
