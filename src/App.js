27a1809050ed60e0938166f0d2f796413ffedcd2
pavlovsky69 <pavlovsky69@gmail.com>
2023-07-13 11:23:39 +0300

    lesson4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import LoginPage from "./pages/LoginPage/LoginPage";
 
 function App() {
   return (
     <div className="App">
-      App
+      <LoginPage/>
     </div>
   );
 }
---
+++ src/pages/LoginPage/LoginPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './LoginPage.scss'
+const LoginPage = () => {
+    return (
+        <div className={styles.wrapper}>
+            
+        </div>
+    );
+};
+
+export default LoginPage;
