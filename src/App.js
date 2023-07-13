66fa425ea509e768ec7191e40bed25e66e49f07a
pavlovsky69 <pavlovsky69@gmail.com>
2023-07-13 17:42:45 +0300

    lesson3 homework

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import React from "react"
 import {useState} from "react";
 import {UsersComponent} from "./UsersComponent/UsersComponent";
 import {Posts} from "./UsersComponent/Posts/Posts";
