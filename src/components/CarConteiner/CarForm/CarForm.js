6f23df16063d66e3585cc3942d1e4bb61294f606
pavlovsky69 <pavlovsky69@gmail.com>
2023-07-14 13:16:05 +0300

    lesson4 homework

--- src/components/CarConteiner/CarForm/CarForm.js
+++ src/components/CarConteiner/CarForm/CarForm.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import {useForm} from "react-hook-form";
 
 const CarForm = () => {
+   const {register, handleSubmit: handleSubmit, reset, formState:{errors, isValid}} = useForm();
+   const save =(data)=>{
+       console.log (data);
+   }
     return (
         <div>
-           CarForm
+            <form onSubmit={handleSubmit(save)}>
+                <label> <input type="text" placeholder={'brand'} {...register('brand')}/></label>
+                <label> <input type="text" placeholder={'price'} {...register('price')}/></label>
+                <label> <input type="text" placeholder={'year'} {...register('year')}/></label>
+                <button>Save</button>
+            </form>
         </div>
     );
 };
