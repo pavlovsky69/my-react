c7ed3e5a9dc8e7e9402f6186b8d832c19cf92d5a
pavlovsky69 <pavlovsky69@gmail.com>
2023-07-19 17:24:02 +0300

    lesson5_part 2 homework

--- src/components/CarContainer/Car/Car.js
+++ src/components/CarContainer/Car/Car.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {Context} from "../CarContainer";
+import {carService} from "../../../services/carService";
 
 const Car = ({car}) => {
-    const {id, brand, price, year}=car;
+    const {setCarForUpdate, setTrigger} = useContext (Context);
+    const {id, brand, price, year} = car;
+    const deleteCar = async () => {
+        try {
+            await carService.deleteById (id)
+        } catch (e) {
+            console.log (e.response.data)
+        } finally {
+            setTrigger ()
+        }
+    }
+
     return (
         <div>
             <div>id:{id}</div>
             <div>brand:{brand}</div>
             <div>price:{price}</div>
             <div>year:{year}</div>
-            <button>update</button>
-            <button>delete</button>
+            <button onClick={() => setCarForUpdate (car)}>update</button>
+            <button onClick={deleteCar}>delete</button>
         </div>
     );
 };
--- src/components/CarContainer/Cars/Cars.js
+++ src/components/CarContainer/Cars/Cars.js
@@ -1,12 +1,14 @@
-import React, {useEffect, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {carService} from "../../../services/carService";
 import {Car} from "../Car/Car";
+import {Context} from "../CarContainer";
 
 const Cars = () => {
     const [cars, setCars] = useState([]);
+    const {trigger} = useContext(Context);
 
     useEffect(()=>{
-carService.getAll().then(({data}) => setCars(data))},[])
+carService.getAll().then(({data}) => setCars(data))},[trigger])
     return (
         <div>
             {cars.map(car=><Car key={car.id} car={car}/>)}
