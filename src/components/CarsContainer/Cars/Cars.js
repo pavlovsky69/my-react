84144845e9ad9a34fc6991b0b3560e315c3cb031
pavlovsky69 <pavlovsky69@gmail.com>
2023-08-17 12:57:38 +0300

    lesson 12 redux toolkit

---
+++ src/components/CarsContainer/Cars/Cars.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Car} from "../Car/Car";
+
+const Cars = () => {
+    return (
+        <div>
+            <Car/>
+        </div>
+    );
+};
+
+export {Cars};
---
+++ src/services/apiService.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+
+import {baseURL} from "../constans";
+
+const apiService = axios.create ({baseURL});
+
+export {apiService};
